845d19a9f566a882713c3e08f2fa3341ca4114b8
Gabriel Grubba <gabriel@discourse.org>
2026-02-02 10:38:33 -0300

    DEV: added node fetch

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,7 +1,11 @@
 import { NavLink } from "react-router";
 import MeteorLogo from "./meteor-logo.svg";
+import { useLoaderData } from "react-router";
 
 export const Header = () => {
+  const gitHubStatus = useLoaderData();
+  console.log('Loader data in Header:', gitHubStatus)
+
   return (
     <div className="header">
       <nav className="nav container">
@@ -9,6 +13,7 @@ export const Header = () => {
           <MeteorLogo className="logo" />
         </div>
       <h1 className="page-title">Welcome to Meteor!</h1>
+      <div className="github-status">GitHub Status: {gitHubStatus}</div>
       </nav>
 
     </div>
--- imports/ui/Routes.jsx
+++ imports/ui/Routes.jsx
@@ -6,6 +6,12 @@ import { About } from "./About";
 export const Routes = createBrowserRouter([
   {
     path: "/",
+    loader: async () => {
+      const response = await fetch('https://www.githubstatus.com/api/v2/summary.json');
+      const data = await response.json();
+      console.log('GitHub status:', data.status.description);
+      return data.status.description;
+    },
     element: <App />,
   },
   {
